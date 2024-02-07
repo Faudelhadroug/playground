7fde6bbbb7c0678b15b81fe8be49117fe8c2784d
Faudel <hadroug.faudel@gmail.com>
2024-02-07 18:55:47 +0100

    test: Auth.service

--- server/src/auth/test/auth.service.spec.ts
+++ server/src/auth/test/auth.service.spec.ts
@@ -1,20 +1,27 @@
-import { UsersService as mockUsersService } from '../../users/__mocks__/users.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '../auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { BadRequestException } from '@nestjs/common';
 import { UsersService } from '../../users/users.service';
 
 describe('AuthService', () => {
   let authService: AuthService;
-  // let usersRepository: UsersRepository;
-  // let userModel: Model<UserDocument>;
+
+  const mockUsersService = {
+    getUserByUsername: jest.fn().mockResolvedValue({
+      username: 'gala',
+      password: 'admin123',
+    }),
+  };
 
   const mockJwtService = {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     sign: jest.fn().mockImplementation((_payload) => {
       return 'mocked_token';
     }),
-    verify: jest.fn().mockImplementation((_token) => {
-      return { sub: 'username' };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    verifyAsync: jest.fn().mockImplementation(async (_token) => {
+      return { username: 'gala' };
     }),
   };
 
@@ -25,18 +32,61 @@ describe('AuthService', () => {
         { provide: UsersService, useValue: mockUsersService },
         { provide: JwtService, useValue: mockJwtService },
       ],
-    })
-      // .overrideProvider(JwtService)
-      // .useValue(mockJwtService)
-      // .overrideProvider(UsersModel)
-      // .useValue(userModel)
-      .compile();
-
+    }).compile();
     authService = module.get<AuthService>(AuthService);
     jest.clearAllMocks();
   });
+  describe('when authServ is initialized', () => {
+    it('then it should be defined', () => {
+      expect(authService).toBeDefined();
+    });
+  });
 
-  it('should be defined', () => {
-    expect(authService).toBeDefined();
+  describe('validateUser', () => {
+    describe('when validateUser is called', () => {
+      it('then it should verify user then return the username of payload', async () => {
+        expect(
+          await authService.validateUser({
+            username: 'gala',
+            password: 'admin123',
+          }),
+        ).toEqual({ username: 'gala' });
+      });
+      it('then it should throw an BadRequestException', async () => {
+        await expect(
+          authService.validateUser({
+            username: 'gala',
+            password: 'adminabc',
+          }),
+        ).rejects.toThrow(BadRequestException);
+      });
+    });
+  });
+
+  describe('sign JWT (createAccessToken & createRefreshToken)', () => {
+    describe('when createAccessToken is called', () => {
+      it('then it should return a signed string', async () => {
+        expect(await authService.createAccessToken('username')).toBe(
+          'mocked_token',
+        );
+      });
+    });
+    describe('when createRefreshToken is called', () => {
+      it('then it should return a signed string', async () => {
+        expect(await authService.createRefreshToken('username')).toBe(
+          'mocked_token',
+        );
+      });
+    });
+  });
+
+  describe('decodeRefreshToken', () => {
+    describe('when decodeRefreshToken is called', () => {
+      it('then it should return a decoded string', async () => {
+        expect(await authService.decodeRefreshToken('oldRefreshToken')).toEqual(
+          { username: 'gala' },
+        );
+      });
+    });
   });
 });
