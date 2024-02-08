530f43333c927bc391d4c3bbb492cfee4c32df12
Faudel <hadroug.faudel@gmail.com>
2024-02-08 18:58:16 +0100

    test: mock auth service

--- server/src/auth/__mocks__/auth.service.ts
+++ server/src/auth/__mocks__/auth.service.ts
@@ -1,6 +1,20 @@
+import { BadRequestException } from '@nestjs/common';
 export const mockAuthService = {
-  validateUser: {},
-  createAccessToken: {},
-  createRefreshToken: {},
-  decodeRefreshToken: {},
+  validateUser: jest.fn().mockImplementation((payload) => {
+    if (payload?.password !== 'admin123' || payload.username !== 'gala')
+      throw new BadRequestException('user or password not matching');
+  }),
+  createAccessToken: jest.fn().mockImplementation((_payload) => {
+    return 'mocked_token';
+  }),
+  createRefreshToken: jest.fn().mockImplementation((_payload) => {
+    return 'mocked_refresh_token';
+  }),
+  decodeRefreshToken: jest.fn().mockImplementation((refreshToken) => {
+    if (refreshToken !== 'mocked_refresh_token')
+      throw new BadRequestException('Invalid refresh token');
+    return {
+      username: 'gala',
+    };
+  }),
 };
